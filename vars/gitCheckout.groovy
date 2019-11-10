95774f85e2518145570170a081820f47626f06d0
shary786 <sharoon.mohideen@gmail.com>
2019-11-10 14:35:00 +0900

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -7,19 +7,19 @@ def call(Map stageParams) {
 //        userRemoteConfigs: [[ url: stageParams.url ]]
  //   ])
  
- checkout([
-      $class: 'GitSCM', 
-      branches: [[name: '*/master']], 
-      doGenerateSubmoduleConfigurations: false, extensions: [], 
-      submoduleCfg: [], 
-      userRemoteConfigs: [[url: 'https://github.com/shary786/jenkins-shared-library-structure.git']]
+// checkout([
+//      $class: 'GitSCM', 
+//      branches: [[name: '*/master']], 
+//      doGenerateSubmoduleConfigurations: false, extensions: [], 
+//      submoduleCfg: [], 
+//      userRemoteConfigs: [[url: 'https://github.com/shary786/jenkins-shared-library-structure.git']]
  
- ])
+// ])
  
-//     checkout([
-//        $class: 'GitSCM',
-//        branches: [[name:  stageParams.branch ]],
-//        userRemoteConfigs: [[ url: stageParams.url ]]
-//    ])
+     checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
  
  }
